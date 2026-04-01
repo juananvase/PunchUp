be65d71c8ae2626e73da771897c4387745cbc2c8
Alexandre Simao <GD75Alexandre@VFS.com>
2026-04-01 00:44:05 -0700

    made groundcheck gravity and punch system first iteration

---
+++ Assets/PunchUp/Characters/Player/Scripts/CustomGravity.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class CustomGravity : MonoBehaviour
+{
+    [field: SerializeField] public float MinGravity { get; private set; } = -1.0f;
+    [field: SerializeField] public float MaxGravity { get; private set; } = -10.0f;
+    [field: SerializeField] public float GravityAcceleration { get; private set; } = -9.0f;
+    public float CurrentGravity { get; private set; }
+    public bool IsGravityEnabled { get; private set; } = true;
+
+    public float HandleGravity()
+    {
+        float gravity = CurrentGravity;
+
+        if (!IsGravityEnabled)
+        {
+            gravity = 0.0f;
+            CurrentGravity = gravity;
+        }
+        else
+        {
+            if (CurrentGravity > MaxGravity)
+            {
+                CurrentGravity += GravityAcceleration * Time.deltaTime;
+                Mathf.Clamp(CurrentGravity, CurrentGravity, MaxGravity);
+            }
+
+            gravity = CurrentGravity;
+        }
+        return gravity;
+    }
+
+    public void DisableGravity()
+    {
+        IsGravityEnabled = false;
+    }
+
+    public void EnableGravity()
+    {
+        IsGravityEnabled = true;
+    }
+}
--- Assets/PunchUp/Characters/Player/Scripts/PlayerMovement.cs
+++ Assets/PunchUp/Characters/Player/Scripts/PlayerMovement.cs
@@ -1,24 +1,45 @@
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody))]
 [RequireComponent(typeof(PlayerInput))]
+[RequireComponent(typeof(CustomGravity))]
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private Rigidbody _rigidBody;
     [SerializeField] private PlayerInput _playerInput;
     [SerializeField] private FirstPersonCamLooker _camLooker;
+    [SerializeField] private CustomGravity _customGravity;
 
     [Header("Locomotion")]
     [SerializeField] private float _maxSpeed = 5.0f;
-    [SerializeField] private float _control = 1.0f;
+    [SerializeField] private float _groundControl = 1.0f;
+    [SerializeField] private float _airControl = 0.5f;
 
     [Header("Turning")]
     [SerializeField] private float _rotationSpeed = 5.0f;
     [SerializeField] private float _turnSpeedMultiplier = 1.0f;
     [SerializeField] private bool _canTurn = true;
 
+    [Header("Ground Check")]
+    [SerializeField] private Vector3 _originOffset;
+    [SerializeField] private float _groundCheckDistance = 2.0f;
+    [SerializeField] private LayerMask _groundLayer;
+    [SerializeField] private float _maxSlopeAngle;
+
+    [Header("Punch")]
+    [SerializeField] private float _punchDistance = 2.0f;
+    [SerializeField] private LayerMask _punchMask;
+    private Vector3 _aimPosition;
+    [SerializeField] private float _punchForce = 10.0f;
+    [SerializeField] private float _punchReflectionForce = 10.0f;
+
+    [Header("Events")]
+    [SerializeField] public UnityEvent OnLanded = new UnityEvent();
+
+    // move inputs
     private bool _hasMoveInput;
     private bool _hasTurnInput;
     private Vector3 _moveInput2D;
@@ -26,11 +47,24 @@ public class PlayerMovement : MonoBehaviour
     private Vector3 _localMoveInput;
     private Vector3 _lookDirection;
 
+    // grounded
+    private Vector3 _groundNormal;
+    private float _lastGroundedTime;
+    private Vector3 _lastGroundedPosition;
+    private bool _hasLanded = false;
+    private bool _wasGroundedLastFrame = false;
+    private bool _isGrounded = false;
+    private float _currentControl;
+
+    // == DEBUG ==
+    private Vector3 _startPositon;
+
     private void OnValidate()
     {
         if(_rigidBody == null) _rigidBody = GetComponent<Rigidbody>();
         if(_playerInput == null) _playerInput = GetComponent<PlayerInput>();
         if(_camLooker == null) _camLooker = GetComponent<FirstPersonCamLooker>();
+        if(_customGravity == null) _customGravity = GetComponent<CustomGravity>();
     }
 
     public void OnMove(InputValue inputValue)
@@ -41,6 +75,21 @@ public class PlayerMovement : MonoBehaviour
     public void OnLook(InputValue inputValue)
     {
         _camLooker.SetLookInput(inputValue.Get<Vector2>());
+    }
+
+    public void OnPunch()
+    {
+        Punch();
+    }
+
+    public void OnJump()
+    {
+        Teleport(_startPositon);
+    }
+
+    private void Start()
+    {
+        _startPositon = transform.position;
     }
 
     private void Update()
@@ -55,17 +104,34 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        _hasLanded = false;
+        _wasGroundedLastFrame = _isGrounded;
+        _isGrounded = GroundCheck();
+        // Called when you land
+        if (!_wasGroundedLastFrame && _isGrounded) Landed();
+
         Vector3 input = _moveInput;
         Vector3 right = Vector3.Cross(transform.up, input);
-        Vector3 forward = Vector3.Cross(right, Vector3.up /*This will be ground normal*/);
+        Vector3 forward = Vector3.Cross(right, _groundNormal);
 
         Vector3 targetVelocity = forward * _maxSpeed;
 
         // TODO: must be consistent rate!
         Vector3 velocityDiff = targetVelocity - _rigidBody.linearVelocity;
         velocityDiff.y = 0;
+        velocityDiff += _groundNormal * _customGravity.HandleGravity();
 
-        _rigidBody.AddForce(velocityDiff * _rigidBody.mass * _control);
+        if (_isGrounded)
+        {
+            _customGravity.DisableGravity();
+        }
+        else if (!_isGrounded)
+        {
+            _customGravity.EnableGravity();
+            _currentControl = _airControl;
+        }
+
+        _rigidBody.AddForce(velocityDiff * _rigidBody.mass * _currentControl);
     }
 
     private void SetMoveInput(Vector3 input)
@@ -109,5 +175,61 @@ public class PlayerMovement : MonoBehaviour
             rotation = Quaternion.Slerp(transform.rotation, targetRotation, _rotationSpeed * _turnSpeedMultiplier * Time.deltaTime);
             _rigidBody.MoveRotation(rotation);
         }
+    }
+
+    private bool GroundCheck()
+    {
+        bool hasHitGround = Physics.Raycast(transform.position + _originOffset, -transform.up, out RaycastHit hitInfo, _groundCheckDistance, _groundLayer);
+        _groundNormal = Vector3.up;
+        if (!hasHitGround) return false;
+
+        Vector3 localGroundNormal = _rigidBody.transform.InverseTransformDirection(hitInfo.normal);
+        float groundSlopeAngle = Vector3.Angle(localGroundNormal, _rigidBody.transform.up);
+
+        if (groundSlopeAngle > _maxSlopeAngle) return false;
+
+        if (hasHitGround && groundSlopeAngle <= _maxSlopeAngle)
+        {
+            _groundNormal = hitInfo.normal;
+            _lastGroundedTime = Time.timeSinceLevelLoad;
+            _lastGroundedPosition = transform.position;
+
+            return true;
+        }
+        return false;
+    }
+
+    private void Landed()
+    {
+        OnLanded.Invoke();
+        _hasLanded = true;
+        _currentControl = _groundControl;
+    }
+
+    private void Punch()
+    {
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out RaycastHit hitInfo, _punchDistance, _punchMask))
+        {
+            _aimPosition = hitInfo.point;
+            Vector3 punchedNormal = hitInfo.normal;
+            Vector3 reflectionVector = transform.position - punchedNormal;
+
+            _currentControl = 0.0f;
+            _rigidBody.AddForce(-Camera.main.transform.forward * _punchReflectionForce, ForceMode.Impulse);
+        }
+    }
+
+    // == DEBUG ==
+    private void Teleport(Vector3 position)
+    {
+        _rigidBody.linearVelocity = Vector3.zero;
+        transform.position = position;
+        _rigidBody.position = position;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position + _originOffset, transform.position + (-transform.up * _groundCheckDistance));
     }
 }
